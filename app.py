c5da3e991c98aa9c56d2bfd4aa23736bac02825a
Carlton D'Silva <22f3001919@ds.study.iitm.ac.in>
2024-08-19 21:11:01 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.title ("TDS Demo")
+
+st.sidebar.header ("User Input")
+
+user_text = st.sidebar.text_input ("Enter some text", "Hello, TDS Students")
+
+number = st.sidebar.slider ("Select a number", min_value=10, max_value=100, value=50)
+
+reverse_text = st.sidebar.checkbox ("Reverse Text")
+
+case = st.sidebar.radio ('Chose the text case:', ('Original', 'Uppercase', 'Lowercase'))
+
+result = number * 5
+
+st.write (f'### The number {number} multiplied by 5 is {result}')
+
+if case == 'Uppercase':
+    transformed_text = user_text.upper()
+elif case == 'Lowercase':
+    transformed_text = user_text.lower()
+else:
+    transformed_text = user_text
+
+if reverse_text:
+    transformed_text = user_text[::-1]
+
+
+st.write ('### Transformed Text')
+st.write (transformed_text)
+
